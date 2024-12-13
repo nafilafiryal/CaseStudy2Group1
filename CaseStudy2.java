455c4558fe121f53d6b54db937ab27030431d957
nafilafiryal <nafilahfiryal05@gmail.com>
2024-12-13 10:30:10 +0700

    nafil

--- CaseStudy2.java
+++ CaseStudy2.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class CaseStudy2 {
     static  int listOrder[][] = new int[4][4];
-    
+    static int row = 0;
 
     static void addOrder () {
         Scanner input = new Scanner(System.in);
@@ -15,22 +15,57 @@ public class CaseStudy2 {
         while (true) {
             System.out.println();
             System.out.println("=====CAFE MENU====");
-            System.out.println("1. Back Caffe - Rp 15.0000");
+            System.out.println("1. Black Caffe - Rp 15.0000");
             System.out.println("2. Latte - Rp. 22.0000");
             System.out.println("3. Pull tea - Rp 12.0000");
-            System.out.println("3. Fried noodles - Rp. 18.000");
+            System.out.println("4. Fried noodles - Rp. 18.000");
             break;
         }
-    }
+
+        int menu = 0;
+        while (true) {
+            System.out.print("Select menu (enter menu number, or 0 to exit): ");
+            menu = input.nextInt();
+            //menu = menuAdd;
+
+            // if (menu <= 4) {
+                switch (menu) {
+                    case 1:
+                        System.out.print("Enter the number of items for black coffe: ");
+                        listOrder[row][0] = input.nextInt();
+                        break;
+                    case 2:
+                        System.out.print("Enter the number of items for latte: ");
+                        listOrder[row][1] = input.nextInt();
+                        break;
+                    case 3:
+                        System.out.print("Enter the number of pull tea: ");
+                        listOrder[row][2] = input.nextInt();
+                        break;
+                    case 4:
+                        System.out.print("Enter the number of fries noodle: ");
+                        listOrder[row][3] = input.nextInt();
+                        break;
+                    default: 
+                        System.out.println("wesss");
+                        return;
+                    }
+                    // } else if (menu == 0) {
+                        //     System.out.println("Order has been recorded.");
+                        // }
+                    }
+                    
+                }
+    
 
     static void showOrderList () {
-
+        
     }
-
+    
     static void Exit () {
-
+        
     }
-
+    
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int menu = 0;
@@ -40,16 +75,35 @@ public class CaseStudy2 {
         System.out.println("3. Exit");
         System.out.print("Select Menu: ");
         menu = input.nextInt();
-
+        
         switch (menu) {
             case 1:
-                addOrder();
-                break;
+            addOrder();
+            break;
             case 2:
             case 3:  
             default:
             
-                break;
+            break;
         }
     }
 }
+// if (menu == 1) {
+//     System.out.print("Enter the number of items for black coffe: ");
+//     listOrder[row][0] = input.nextInt();
+//     break;
+// } if (menu == 2) {
+//     System.out.print("Enter the number of items for latte: ");
+//     listOrder[row][1] = input.nextInt();
+//     break;
+// } if (menu == 3) {
+//     System.out.print("Enter the number of pull tea: ");
+//     listOrder[row][2] = input.nextInt();
+//     break;
+// } if (menu == 4) {
+//     System.out.print("Enter the number of fries noodle: ");
+//     listOrder[row][3] = input.nextInt();
+//     break;
+// } if (menu == 0) {
+//     System.out.println("Order added successfully.");
+// }
