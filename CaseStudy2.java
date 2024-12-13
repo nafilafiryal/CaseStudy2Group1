cdccdc77128867d3cc004e3980678347715e3253
aiska28 <aiskacaca8@gmail.com>
2024-12-13 08:44:38 +0700

    aiska

--- CaseStudy2.java
+++ CaseStudy2.java
@@ -1,6 +1,35 @@
 import java.util.Scanner;
 
 public class CaseStudy2 {
+    static  int listOrder[][] = new int[4][4];
+    
+
+    static void addOrder () {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter customer's name: ");
+        String name = input.nextLine();
+        System.out.print("Enter Table Number: ");
+        int tabelNumber = input.nextInt();
+
+        while (true) {
+            System.out.println();
+            System.out.println("=====CAFE MENU====");
+            System.out.println("1. Back Caffe - Rp 15.0000");
+            System.out.println("2. Latte - Rp. 22.0000");
+            System.out.println("3. Pull tea - Rp 12.0000");
+            System.out.println("3. Fried noodles - Rp. 18.000");
+            break;
+        }
+    }
+
+    static void showOrderList () {
+
+    }
+
+    static void Exit () {
+
+    }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
@@ -14,12 +43,12 @@ public class CaseStudy2 {
 
         switch (menu) {
             case 1:
-                
+                addOrder();
                 break;
             case 2:
-            case 3:
-            case 4:    
+            case 3:  
             default:
+            
                 break;
         }
     }
