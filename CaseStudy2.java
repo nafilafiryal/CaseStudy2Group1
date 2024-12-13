3c8e95db264c14f6c3742f4cf382cf4a78f2e88e
nafilafiryal <nafilahfiryal05@gmail.com>
2024-12-13 08:11:47 +0700

    yuhu

--- CaseStudy2.java
+++ CaseStudy2.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class CaseStudy2 {
+
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        int menu = 0;
+        System.out.println("===== MAIN MENU =====");
+        System.out.println("1. Add Order");
+        System.out.println("2. Show Order List");
+        System.out.println("3. Exit");
+        System.out.print("Select Menu: ");
+        menu = input.nextInt();
+
+        switch (menu) {
+            case 1:
+                
+                break;
+            case 2:
+            case 3:
+            case 4:    
+            default:
+                break;
+        }
     }
 }
